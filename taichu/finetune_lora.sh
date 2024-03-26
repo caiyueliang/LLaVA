739bfba4bbd796578e5bbc0a17209b518699f957
caiyueliang <393900414@qq.com>
2024-03-26 10:15:56 +0800

    update

--- taichu/finetune_lora.sh
+++ taichu/finetune_lora.sh
@@ -4,6 +4,14 @@
 pretrained_model_path="/mnt/publish-data/pretrain_models/llava/llava-v1.6-vicuna-7b/"
 vision_tower="/mnt/publish-data/pretrain_models/llava/clip-vit-large-patch14-336/"
 output_path="./output/"
+data_path=""
+num_train_epochs=1
+per_device_train_batch_size=4
+per_device_eval_batch_size=4
+gradient_accumulation_steps=1
+learning_rate=2e-4
+model_max_length=2048
+
 
 # 遍历所有传递给脚本的参数
 while [[ $# -gt 0 ]]; do
@@ -23,10 +31,45 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
-
+    --data_path)
+      data_path="$2"
+      shift
+      shift
+      ;;
+    --num_train_epochs)
+      num_train_epochs="$2"
+      shift
+      shift
+      ;;
+    --per_device_train_batch_size)
+      per_device_train_batch_size="$2"
+      shift
+      shift
+      ;;
+    --per_device_eval_batch_size)
+      per_device_eval_batch_size="$2"
+      shift
+      shift
+      ;;
+    --gradient_accumulation_steps)
+      gradient_accumulation_steps="$2"
+      shift
+      shift
+      ;;
+    --learning_rate)
+      learning_rate="$2"
+      shift
+      shift
+      ;;
+    --model_max_length)
+      model_max_length="$2"
+      shift
+      shift
+      ;;
     *)    # 对于不认识的选项，直接报错或者忽略
       echo "unknow params: $1"
-      exit 1
+      shift
+      # exit 1
       ;;
   esac
 done
@@ -36,21 +79,22 @@ echo "======================================================================"
 echo "[pretrained_model_path]: ${pretrained_model_path}"
 echo "[vision_tower]: ${vision_tower}"
 echo "[output_path]: ${output_path}"
+echo "[data_path]: ${data_path}"
+echo "[num_train_epochs]: ${num_train_epochs}"
+echo "[per_device_train_batch_size]: ${per_device_train_batch_size}"
+echo "[per_device_eval_batch_size]: ${per_device_eval_batch_size}"
+echo "[gradient_accumulation_steps]: ${gradient_accumulation_steps}"
+echo "[learning_rate]: ${learning_rate}"
+echo "[model_max_length]: ${model_max_length}"
 echo "======================================================================"
 
-# HOST=${HOST:=0.0.0.0}
-# PORT=${PORT:=8080}
-# CONTROLLER_PORT=${CONTROLLER_PORT:=10000}
-# WEB_PORT=${WEB_PORT:=8081}
-# MODEL_PATH=${MODEL_PATH:=/mnt/publish-data/pretrain_models/llava/llava-v1.6-vicuna-7b/}
-
-deepspeed llava/train/train_mem.py \
+CUDA_VISIBLE_DEVICES=7 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
     --pretrained_model_path ${pretrained_model_path} \
     --vision_tower ${vision_tower} \
     --output_path ${output_path} \
-    --data_path "/mnt/publish-data/train_data/llava_data/01/" \
+    --data_path ${data_path} \
     --version v1 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -59,21 +103,21 @@ deepspeed llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --num_train_epochs ${num_train_epochs} \
+    --per_device_train_batch_size ${per_device_train_batch_size} \
+    --per_device_eval_batch_size ${per_device_eval_batch_size} \
+    --gradient_accumulation_steps ${gradient_accumulation_steps} \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
-    --learning_rate 2e-4 \
+    --learning_rate ${learning_rate} \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 2048 \
+    --model_max_length ${model_max_length} \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
